aac72373952ca8ccb00c1214db998e9d47997241
Emil Mattsson <emilmattsson14@gmail.com>
2022-11-28 15:39:11 +0100

    styling added

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./Board.scss";
 
 export default function Board() {
-  return <div className="board">Board</div>;
+  return (
+    <div className="board">
+      <div className="board-column">1</div>
+      <div className="board-column">2</div>
+      <div className="board-column">3</div>
+    </div>
+  );
 }
