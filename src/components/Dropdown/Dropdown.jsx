5e602c820d32bf0e6632850cb4398f7d80425035
Emil Mattsson <emilmattsson14@gmail.com>
2022-12-01 23:43:58 +0100

    adding light/dark mode toggle

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import Links from "../Links/Links";
-// import "./Dropdown.scss";
-// import CrossIcon from "../../assets/icon-cross.svg";
-
-// export default function Dropdown({ dropdown, toggleDropdown }) {
-//   return (
-//     <div className={`dropdown ${dropdown ? "active-dropdown" : ""}`}>
-//       <div className="dropdown-box">
-//         <button
-//           className="dropdown-close-btn"
-//           onClick={() => {
-//             toggleDropdown(false);
-//           }}
-//         >
-//           <img src={CrossIcon} alt="" />
-//         </button>
-//         <Links />
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 
 import "./Dropdown.scss";
---
+++ src/components/Lightmode/Lightmode.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Lightmode() {
+  return (
+    <div>
+      <button onClick={hej}>Toggle</button>
+    </div>
+  );
+}
